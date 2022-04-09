f047c0a2dd5a441e48a49dd6f7bbc21c464673fb
EunseokOh <lspk.lpg@gmail.com>
2022-04-09 17:25:59 +0900

    original code

---
+++ design-pattern-practice-in-csharp/4_BuilderPattern/Original/App.cs
@@ -0,0 +1,20 @@
+﻿namespace design_pattern_practice_in_csharp_singleton._4_BuilderPattern.Original;
+
+public class App
+{
+    public static void Main(string[] args)
+    {
+        TourPlan tourPlan = new();
+        tourPlan.Title = "China Trip";
+        tourPlan.Nights = 2;
+        tourPlan.Days = 3;
+        tourPlan.StartDate = new DateTime(2022, 04, 08);
+        tourPlan.WhereToStay = "Resort";
+        tourPlan.AddPlan(0, "do something");
+        tourPlan.AddPlan(0, "do something2");
+        tourPlan.AddPlan(1, "do something3");
+        tourPlan.AddPlan(3, "checkout");
+
+        Console.WriteLine(tourPlan.ToString());
+    }
+}
