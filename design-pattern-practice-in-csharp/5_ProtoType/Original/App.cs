c66cf56fae2e0a1f491b6064f5259f7d7bc96a82
EunseokOh <lspk.lpg@gmail.com>
2022-04-09 21:03:42 +0900

    prototype pattern

---
+++ design-pattern-practice-in-csharp/5_ProtoType/Original/App.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+
+namespace design_pattern_practice_in_csharp_singleton._5_ProtoType.Original;
+
+public class App
+{
+    public static void Main(string[] args)
+    {
+        GithubRepository repository = new();
+        repository.User = "es5h";
+        repository.Name = "design-pattern-practice";
+
+        GithubIssue githubIssue = new GithubIssue(repository);
+        githubIssue.Id = 1;
+        githubIssue.Title = "how to use proto-type pattern";
+
+        string url = githubIssue.Url;
+        Console.WriteLine(url);
+
+        GithubIssue clone = (GithubIssue) githubIssue.Clone();
+        
+        Console.WriteLine(clone != githubIssue);
+        Console.WriteLine(clone.Equals(githubIssue));
+        Console.WriteLine(clone.GetType() == githubIssue.GetType());
+    }
+}
