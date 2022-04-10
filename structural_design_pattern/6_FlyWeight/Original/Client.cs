3ca0365e91382872a9b27ba3f9ae51c2c3418d82
EunseokOh <lspk.lpg@gmail.com>
2022-04-10 16:32:51 +0900

    flyweight pattern

--- structural_design_pattern/6_FlyWeight/Original/Client.cs
+++ structural_design_pattern/6_FlyWeight/Original/Client.cs
@@ -7,10 +7,10 @@ public class Client
         
         // extrinsic, intrinsic 을 분ㄹ히ㅏ여 재사용성으로 메모리를 줄인다. 인스턴스 많을 때 사용
         
-        Character c1 = new Character("h", "white", "Nanum", 13);
-        Character c2 = new Character("e", "white", "Nanum", 13);
-        Character c3 = new Character("l", "white", "Nanum", 13);
-        Character c4 = new Character("l", "white", "Nanum", 13);
-        Character c5 = new Character("o", "white", "Nanum", 13);
+        Character c1 = new Character('h', "white", "Nanum", 13);
+        Character c2 = new Character('e', "white", "Nanum", 13);
+        Character c3 = new Character('l', "white", "Nanum", 13);
+        Character c4 = new Character('l', "white", "Nanum", 13);
+        Character c5 = new Character('o', "white", "Nanum", 13);
     }
 }
