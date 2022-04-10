5be2debef0596e8f28a88e00ab6de0b4dc53f4ce
EunseokOh <lspk.lpg@gmail.com>
2022-04-10 17:25:32 +0900

    lazy loading

--- structural_design_pattern/7_Proxy/AfterPattern_2/GameServiceProxy.cs
+++ structural_design_pattern/7_Proxy/AfterPattern_2/GameServiceProxy.cs
@@ -4,16 +4,17 @@ namespace structural_design_pattern._7_Proxy.AfterPattern_2;
 
 public class GameServiceProxy : IGameService
 {
-    private readonly IGameService _gameService;
+    private IGameService _gameService;
 
-    public GameServiceProxy(IGameService gameService)
-    {
-        _gameService = gameService;
-    }
 
     public void StartGame()
     {
         var sw = Stopwatch.StartNew();
+        if (_gameService == null)
+        {
+            _gameService = new DefaultGameService();
+        }
+
         _gameService.StartGame();
         sw.Stop();
         Console.WriteLine(sw.ElapsedMilliseconds.ToString());
