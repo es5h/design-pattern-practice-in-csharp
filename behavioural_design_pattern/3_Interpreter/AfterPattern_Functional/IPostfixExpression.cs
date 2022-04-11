08cb7d2ba72ceb2dece614473dddb1a9a0e9c9e6
EunseokOh <lspk.lpg@gmail.com>
2022-04-12 01:39:34 +0900

    refactor the name of object

--- behavioural_design_pattern/3_Interpreter/AfterPattern_Functional/IPostfixExpression.cs
+++ behavioural_design_pattern/3_Interpreter/AfterPattern_Functional/IPostfixExpression.cs
@@ -1,14 +1,18 @@
 ﻿namespace behavioural_design_pattern._3_Interpreter.AfterPattern_Functional;
 public class PostfixExpression
 {
-    private PostfixExpression(Func<IDictionary<string, int>, int> interpret)
+    private PostfixExpression(Func<IDictionary<string, int>, int> interpretDelegate)
     {
-        Interpret = interpret;
+        InterpretDelegate = InterpretDelegate;
     }
-
-    private Func<IDictionary<string, int>, int> Interpret { get; init; }
-    public int DoInterpret(IDictionary<string, int> context) => Interpret(context);
-    public static PostfixExpression Plus(PostfixExpression left, PostfixExpression right) => new PostfixExpression(interpret: context => left.DoInterpret(context) + right.DoInterpret(context));
-    public static PostfixExpression Minus(PostfixExpression left, PostfixExpression right) => new PostfixExpression(interpret: context => left.DoInterpret(context) - right.DoInterpret(context));
-    public static PostfixExpression Variable(string c) => new PostfixExpression(interpret: context => context[c]);
+    
+    private Func<IDictionary<string, int>, int> InterpretDelegate { get; init; }
+    public int Interpret(IDictionary<string, int> context) => InterpretDelegate(context);
+    
+    public static PostfixExpression Plus(PostfixExpression left, PostfixExpression right) 
+        => new PostfixExpression(context => left.Interpret(context) + right.Interpret(context));
+    public static PostfixExpression Minus(PostfixExpression left, PostfixExpression right) 
+        => new PostfixExpression(context => left.Interpret(context) - right.Interpret(context));
+    public static PostfixExpression Variable(string c) 
+        => new PostfixExpression( context => context[c]);
 }
