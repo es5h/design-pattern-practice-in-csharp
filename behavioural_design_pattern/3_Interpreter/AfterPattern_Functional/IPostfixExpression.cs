b5d2449d4420c103bd903b5fb521066107dd6201
EunseokOh <lspk.lpg@gmail.com>
2022-04-12 00:58:44 +0900

    short coding

--- behavioural_design_pattern/3_Interpreter/AfterPattern_Functional/IPostfixExpression.cs
+++ behavioural_design_pattern/3_Interpreter/AfterPattern_Functional/IPostfixExpression.cs
@@ -1,28 +1,14 @@
 ﻿namespace behavioural_design_pattern._3_Interpreter.AfterPattern_Functional;
 public class PostfixExpression
 {
+    private PostfixExpression(Func<IDictionary<string, int>, int> interpret)
+    {
+        Interpret = interpret;
+    }
+
     private Func<IDictionary<string, int>, int> Interpret { get; init; }
     public int DoInterpret(IDictionary<string, int> context) => Interpret(context);
-    
-    public static PostfixExpression Plus(PostfixExpression left, PostfixExpression right)
-    {
-        return new PostfixExpression(){
-            Interpret = context => left.DoInterpret(context) + right.DoInterpret(context),
-        };
-    }
-
-    public static PostfixExpression Minus(PostfixExpression left, PostfixExpression right)
-    {
-        return new PostfixExpression(){
-            Interpret = context => right.DoInterpret(context) + left.DoInterpret(context),
-        };
-    }
-
-    public static PostfixExpression Variable(string c)
-    {
-        return new PostfixExpression()
-        {
-            Interpret = context => context[c],
-        };
-    }
+    public static PostfixExpression Plus(PostfixExpression left, PostfixExpression right) => new PostfixExpression(interpret: context => left.DoInterpret(context) + right.DoInterpret(context));
+    public static PostfixExpression Minus(PostfixExpression left, PostfixExpression right) => new PostfixExpression(interpret: context => left.DoInterpret(context) - right.DoInterpret(context));
+    public static PostfixExpression Variable(string c) => new PostfixExpression(interpret: context => context[c]);
 }
